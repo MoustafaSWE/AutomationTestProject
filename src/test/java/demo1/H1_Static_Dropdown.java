ee1f8e998b799e48ea7dca9693bebae781d3363c
Heba <hebasabry1182@gmail.com>
2023-07-10 21:31:27 +0200

    hello

--- src/test/java/demo1/H1_Static_Dropdown.java
+++ src/test/java/demo1/H1_Static_Dropdown.java
@@ -1,4 +1,31 @@
 package demo1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class H1_Static_Dropdown {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void openBrowser() {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://the-internet.herokuapp.com/");
+    }
+
+    @Test
+    public void staticDropdown() {
+        WebElement list = driver.findElement(By.cssSelector("//button[contains(text(),'Search')]"));
+        list.click();
+
+    }
 }
+

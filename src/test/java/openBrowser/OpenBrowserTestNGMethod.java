083ba497108053bfdc4250d45cf3d4f75ddcc01f
fatma <fatma.tayseer@gmail.com>
2023-07-10 22:12:53 +0300

    fatma

--- src/test/java/openBrowser/OpenBrowserTestNGMethod.java
+++ src/test/java/openBrowser/OpenBrowserTestNGMethod.java
@@ -1,11 +1,36 @@
 package openBrowser;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class OpenBrowserTestNGMethod {
 
+    WebDriver driver ;
+
+    @BeforeTest
+    public void open_url() {
+        driver =new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/");
+
+
+    }
+
     @Test
     public void openBrowser (){
+
+
+        WebElement drop = driver.findElement(By.);
+        drop.click();
+
+
+        Select list = new Select(.....);
+        list.selectByValue("1");
 
     }
 }
